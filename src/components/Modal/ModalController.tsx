5531c8d72de1eea778cf63ca9dcf8d06010909aa
HuannAnd <huannvicente14@outlook.com>
2024-07-05 12:26:22 -0300

    feat: creating modals, config: installing new dep. react-router-dom

---
+++ src/components/Modal/ModalController.tsx
@@ -0,0 +1,16 @@
+import { useLocation } from "react-router-dom";
+
+
+interface Props { key: string }
+export default function ModalController({  }: Props){
+  const location = useLocation()
+  const pathname = location.
+
+  switch (location) {
+    case "donations":
+      return 
+
+    default:
+      break;
+  }
+}
---
+++ src/components/Modal/ModalDonation.tsx
@@ -0,0 +1,10 @@
+
+
+interface Props {}
+export default function ModalDonation() {
+  return (
+   <dialog className={styles}>
+
+   </dialog> 
+  )
+}
